5cf7912245b8d96a6e0ccdf11ffa8492a3567485
david <david@172.27.128.1>
2024-11-12 16:36:28 -0600

    Nueva interfaz de inicio runnable con el jar correspondiente

--- src/main/java/com/securitysistem/sistemadeseguridad/SistemaDeSeguridad/Formulario1.java
+++ src/main/java/com/securitysistem/sistemadeseguridad/SistemaDeSeguridad/Formulario1.java
@@ -2,7 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package Interfaz;
+package com.securitysistem.sistemadeseguridad.SistemaDeSeguridad;
+
 
 import com.formdev.flatlaf.intellijthemes.FlatArcDarkIJTheme;
 import javax.swing.UIManager;
